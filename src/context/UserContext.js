1f1a5f3aafeabf2e6f3dfef2dde9d0329ee14689
Ritesh Kumar <98106470+riteshkrofficial@users.noreply.github.com>
2024-03-07 11:39:17 +0530

    Add files via upload

---
+++ src/context/UserContext.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+
+// The createContext function in React is used to create a Context object.
+//  Context provides a way to pass data through the component tree without having 
+//  to pass props down manually at every level.
